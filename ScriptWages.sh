9ac2c7c6dc5234f64551928108823bb7ce51199d
Madeline Hightower <mhighto1@nd.edu>
2019-10-09 18:28:43 -0400

    fixed part 2 errors

--- ScriptWages.sh
+++ ScriptWages.sh
@@ -7,6 +7,6 @@ cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "male" | sort -n -k 2 
 
 
 
-cat wages.csv | tail -n+2 | sort -n -k 4 | grep "female" | head -n 1
-cat wages.csv | tail -n+2 | sort -n -k 4| grep "female" | tail -n 1
-cat wages.csv | tail -n+2 | sort -n k 4 | head -n 10    
+cat wages.csv | tail -n+2 | sort -n -k 4 | head -n 1
+cat wages.csv | tail -n+2 | sort -n -k 4 | tail -n 1
+cat wages.csv | tail -n+2 | sort -n -k 4 | head -n 10 | grep -c  "female"   
