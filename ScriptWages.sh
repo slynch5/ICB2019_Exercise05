36e953ae4bd4831959be9ac9f86bbf660ff8e4ce
Samantha Lynch <slynch5@nd.edu>
2019-10-09 19:19:18 -0400

    Fixed bash errors

--- ScriptWages.sh
+++ ScriptWages.sh
@@ -1,4 +1,4 @@
-x# Exercise 5 Shell Script 
+# Exercise 5 Shell Script 
 
 # Usage: bash ScriptWages.sh 
 
@@ -12,8 +12,6 @@ cat wages.csv | tail -n+2 | sed 's/,/ /g'| cut -d " " -f 1-2,4 | sort -n -k 3 | 
 cat wages.csv | tail -n+2 | sed 's/,/ /g'| cut -d " " -f 1-2,4 | sort -n -k 3 | head -n 10 | egrep -c "fe"   
 
 
-val1=$(cat wages.csv | tail -n+2 | sed 's/,/ /g'| cut -d " " -f 3-4 | egrep "16 "
-| sort -n -k 2 | head -n 1 | cut -d " " -f 2)
-val2=$(cat wages.csv | tail -n+2 | sed 's/,/ /g'| cut -d " " -f 3-4 | egrep "12$
-| sort -n -k 2 | head -n 1 | cut -d " " -f 2)
-echo "$val1-val2" | bc
+val1=$(cat wages.csv | tail -n+2 | sed 's/,/ /g'| cut -d " " -f 3-4 | egrep "16 "| sort -n -k 2 | head -n 1 | cut -d " " -f 2)
+val2=$(cat wages.csv | tail -n+2 | sed 's/,/ /g'| cut -d " " -f 3-4 | egrep "12 "| sort -n -k 2 | head -n 1 | cut -d " " -f 2)
+echo "$val1-$val2" | bc
