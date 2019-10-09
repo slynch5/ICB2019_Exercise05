70839e32f8e96232bd0754321f8c570b0fb0d21d
Samantha Lynch <slynch5@nd.edu>
2019-10-09 18:10:05 -0400

    Part One

--- ScriptWages.sh
+++ ScriptWages.sh
@@ -1,6 +1,6 @@
 # Exercise 5 Shell Script 
 
-# Usage: 
+# Usage: bash ScriptWages.sh 
 
 cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "female" | sort -n -k 2 
 cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "male" | sort -n -k 2  
