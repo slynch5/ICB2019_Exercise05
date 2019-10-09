418a86a91a78255f95c9f67e41bda94663795891
Samantha Lynch <slynch5@nd.edu>
2019-10-09 18:24:10 -0400

    Fixed Part One

--- ScriptWages.sh
+++ ScriptWages.sh
@@ -2,8 +2,8 @@
 
 # Usage: bash ScriptWages.sh 
 
-cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "female" | sort -n -k 2 
-cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "male" | sort -n -k 2  
+cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "female" | sort -n -k 2 > Part1.txt   
+cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "male" | sort -n -k 2 >> Part1.txt
 
 
 
