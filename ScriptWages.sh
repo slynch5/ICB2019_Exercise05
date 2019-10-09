7e14938aa26390856dd58d58ab1401a5ca56bd62
Samantha Lynch <slynch5@nd.edu>
2019-10-09 18:08:56 -0400

    ScriptWages.sh

--- ScriptWages.sh
+++ ScriptWages.sh
@@ -2,4 +2,8 @@
 
 # Usage: 
 
+cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "female" | sort -n -k 2 
+cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "male" | sort -n -k 2  
+
+
 
