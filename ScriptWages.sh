4cc8382c2cc8651a3c3a2707d1b352af77969710
Madeline Hightower <mhighto1@nd.edu>
2019-10-09 18:21:17 -0400

    added script for part 2

--- ScriptWages.sh
+++ ScriptWages.sh
@@ -7,4 +7,6 @@ cat wages.csv | sed 's/,/ /g' | cut -d " " -f 1-2 | egrep "male" | sort -n -k 2
 
 
 
-cat wages.csv | cut "," -f 1,4 | sort -n -k 2 |   
+cat wages.csv | tail -n+2 | sort -n -k 4 | grep "female" | head -n 1
+cat wages.csv | tail -n+2 | sort -n -k 4| grep "female" | tail -n 1
+cat wages.csv | tail -n+2 | sort -n k 4 | head -n 10    
